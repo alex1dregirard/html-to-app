b1005b2ef62f54ddefe7de9a19cfa20d7ade8bb9
Alexandre GIRARD <alexandre.girard@maif.fr>
2023-09-19 17:45:59 +0200

    ✨ Modifie le titre de la page avec js
    🎨 Améliore la gestion du titre

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log('Hello World')
+
+
+changeTitle();
+
+/**
+ * Change the title of the page
+ */
+function changeTitle() {
+    const title = document.getElementById('title');
+    title.innerText += ' from JS';
+    title.style.color = 'red';
+}
